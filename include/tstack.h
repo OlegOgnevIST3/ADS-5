5264b0eaf54bcec0bcd46a87a6db649766cd416f
OlegOgnevIST3 <124896310+OlegOgnevIST3@users.noreply.github.com>
2023-04-08 23:06:42 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -8,10 +8,8 @@ class TStack {
  private:
   T arr[100];
   int top;
-  
- public:
-  TStack() :top(-1) {}
-  
+  public:
+  TStack() :top(-1) {} 
   void push(T value) {
     if (isFull()) {
       throw std::string("Stack is full");
